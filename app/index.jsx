6faa99c0d439e6a4bb2b93ff885cb09252860d8f
Andrew Emmanuel Anim <andrew.eanim@gmail.com>
2024-09-24 19:41:48 +0200

    made changes to some files

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import {Link} from 'expo-router';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Paste</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Paste</Text>
             <StatusBar style="auto"/>
 
             {/* Links you to another screen/page */}
@@ -13,13 +13,3 @@ export default function App() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    },
-});
